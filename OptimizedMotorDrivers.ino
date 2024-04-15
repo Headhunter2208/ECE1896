39d959d921c6968c4bfde9d144e1665db216f4f4
headhunter22 <82061826+headhunter22@users.noreply.github.com>
2024-04-15 02:04:55 -0400

    Update OptimizedMotorDrivers.ino
    Most up-to-date code. Changes include: homing motor functions, limit switches and hall sensors, other miscellaneous edits with constants, etc

--- OptimizedMotorDrivers.ino
+++ OptimizedMotorDrivers.ino
@@ -1,6 +1,9 @@
 #include <math.h>
 #include <digitalWriteFast.h>
 #include <Encoder.h>
+#include <Servo.h>
+
+Servo sv;
 
 // Declare constant multipliers and variables used for testing
 const int StepsPerDegree = 8;
@@ -10,30 +13,37 @@ unsigned long TimeDelayMilliAFTER;
 unsigned long TimeDelayMilliMOVE;
 
 // Declare Direction pins for each motor
-const int XDir = 5;
-const int YDir = 6;
-const int ZDir = 0;
-const int ADir = 0;
-const int BDir = 0;
-const int CDir = 0;
+const int XDir = 39;
+const int YDir = 31;
+const int ZDir = 33;
+const int ADir = 35;
+const int BDir = 37;
+const int CDir = 41;
 const int GripDir = 0;
 
 // Declare driving step pins for all motors
-const int XStepPin = 2;
-const int YStepPin = 3;
-const int ZStepPin;
-const int AStepPin;
-const int BStepPin;
-const int CStepPin;
+const int XStepPin = 29;
+const int YStepPin = 30;
+const int ZStepPin = 32;
+const int AStepPin = 34;
+const int BStepPin = 36;
+const int CStepPin = 40;
 const int GripStepPin;
+
+const int XLimitPin = 7;
+const int YLimitPin = 8;
+const int ZLimitPin = 9;
+const int ALimitPin = 42;
+const int RollLimitPin = 43;
+const int TiltLimitPin = 44;
 
 // Declare constant gear ratio multipliers for moving motors
 const float xGearRatioMult = 13.5;
 const float yGearRatioMult = 150;
 const float zGearRatioMult = 150;
 const float aGearRatioMult = 48;
-const float bGearRatioMult = 67.82;
-const float cGearRatioMult = 67.82;
+const float bGearRatioMult = 68;
+const float cGearRatioMult = 68;
 
 // Create struct that holds all joint angle positions
 struct State {
@@ -65,8 +75,10 @@ void setup() {
   pinMode(BDir, OUTPUT);
   pinMode(CStepPin, OUTPUT);
   pinMode(CDir, OUTPUT);
-  pinMode(GripStepPin, OUTPUT);
-  pinMode(GripDir, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, INPUT);
+
+  sv.attach(6);
 
   // Init starting states for each struct
   sInitial.X = 0;
@@ -86,6 +98,15 @@ void setup() {
   // Init serial data connections
   Serial.begin(115200);
   Serial.println("Begin Serial");
+
+  sv.write(175);
+  HomeMotorsLIMITSWITCH(XStepPin, XDir, XLimitPin, 1);
+  HomeMotorsLIMITSWITCH(YStepPin, YDir, YLimitPin, 1);
+  HomeMotorsLIMITSWITCH(ZStepPin, ZDir, ZLimitPin, 1);
+  HomeMotorsENCODER(AStepPin, ADir, ALimitPin, 1);
+  HomeMotorsROLLTILT(BStepPin, CStepPin, BDir, CDir, RollLimitPin, 0, 0);
+  HomeMotorsROLLTILT(BStepPin, CStepPin, BDir, CDir, TiltLimitPin, 1, 1);
+  Serial.println("End of setup");
 }
 
 void loop() {
@@ -99,7 +120,7 @@ void loop() {
     // Parse Incoming joint data via serial connection
     sFinal = ParseString(ReadInSerial);
 
-    // Output latency time from 
+    // Output latency time from
     TimeDelayMilliAFTER = millis();
     Serial.println(TimeDelayMilliAFTER - TimeDelayMilliBEFORE);
 
@@ -110,7 +131,7 @@ void loop() {
     moveStepper(AStepPin, ADir, sFinal.A, sInitial.A, aGearRatioMult);
     moveStepper(BStepPin, BDir, sFinal.B, sInitial.B, bGearRatioMult);
     moveStepper(CStepPin, CDir, sFinal.C, sInitial.C, cGearRatioMult);
-    moveStepper(GripStepPin, GripDir, sFinal.Grip, sInitial.Grip, 1.0);
+    moveServo(sFinal.Grip);
 
     //Update states for next iterations of movements
     sInitial = UpdateStates(sFinal);
@@ -195,6 +216,16 @@ void moveStepper(int StepPin, int StepDir, int sFinal, int sInitial, float GearR
   }
 }
 
+void moveServo(int s) {
+  if (s == 1) {
+    sv.write(145);
+  }
+
+  if (s == 0) {
+    sv.write(175);
+  }
+}
+
 State UpdateStates(State sFinal) {
   State s;
   s.X = sFinal.X;
@@ -208,7 +239,7 @@ State UpdateStates(State sFinal) {
   return s;
 }
 
-void PrintLocation(State s){
+void PrintLocation(State s) {
   Serial.print(s.X);
   Serial.print(", ");
   Serial.print(s.Y);
@@ -225,14 +256,57 @@ void PrintLocation(State s){
   Serial.print("\n");
 }
 
-void HomeMotors(int MotorPin, int DirPin) {
+void HomeMotorsLIMITSWITCH(int MotorPin, int DirPin, int readPin, int direction) {
+  if(direction == 1){
   digitalWrite(DirPin, LOW);  // low = counter-clockwise
-  int t = 0;
-  while (t < 360 * StepsPerDegree * DegreesOverSteps) {
+  }
+  else {digitalWrite(DirPin, HIGH);
+  }
+  while (digitalRead(readPin) == LOW) {
     digitalWriteFast(MotorPin, HIGH);
-    delay(1);
+    delayMicroseconds(700);
     digitalWriteFast(MotorPin, LOW);
-    delay(1);
-    t += 1;
+    delayMicroseconds(700);
+  }
+}
+
+void HomeMotorsENCODER(int MotorPin, int DirPin, int readPin, int direction) {
+    if(direction == 1){
+  digitalWrite(DirPin, LOW);  // low = counter-clockwise
+  }
+  else {digitalWrite(DirPin, HIGH);
+  }
+
+  while (digitalRead(readPin) == HIGH) {
+    digitalWriteFast(MotorPin, HIGH);
+    delayMicroseconds(700);
+    digitalWriteFast(MotorPin, LOW);
+    delayMicroseconds(700);
+  }
+}
+
+void HomeMotorsROLLTILT(int MotorPin1, int MotorPin2, int DirPin1, int DirPin2, int readPin, int direction1, int direction2) {
+    if(direction1 == 1){
+  digitalWrite(DirPin1, LOW);  // low = counter-clockwise
+  }
+  else {digitalWrite(DirPin1, HIGH);
+  }
+
+      if(direction2 == 1){
+  digitalWrite(DirPin2, LOW);  // low = counter-clockwise
+  }
+  else {digitalWrite(DirPin2, HIGH);
+  }
+
+  while (digitalRead(readPin) == HIGH) {
+    digitalWriteFast(MotorPin1, HIGH);
+    delayMicroseconds(350);
+    digitalWriteFast(MotorPin1, LOW);
+    delayMicroseconds(350);
+
+    digitalWriteFast(MotorPin2, HIGH);
+    delayMicroseconds(350);
+    digitalWriteFast(MotorPin2, LOW);
+    delayMicroseconds(350);
   }
 }
